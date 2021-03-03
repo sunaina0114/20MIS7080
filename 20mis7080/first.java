c6342252b4f62016949cfeb0811ed605c34ca535
harsha <harshavardhan.20mis7038@vitap.ac.in>
2021-03-03 17:00:17 +0530

    third file

--- 20mis7080/first.java
+++ 20mis7080/first.java
@@ -1,2 +1,9 @@
-//swe1002 lab 1
-
+  import java.util.Scanner;
+  public class Main {
+	  public static void main(String[] args) {
+		  Scanner sc = new Scanner (System.in);
+		 System.out.print("enter a number: ") ;
+		 int number = sc.nextInt();
+		 System.out.println("your number: " + number):
+	  }
+  }
